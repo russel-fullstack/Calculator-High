34e318ee64ec14d8fa42a6a44d3dca919619e8b3
russel-fullstack <russeldev95@gmail.com>
2026-02-20 09:39:39 +0100

    calculator end

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const buttons = document.querySelectorAll(".btn");
 const result = document.querySelector(".result");
 const clear1 = document.getElementById("clear1");
 const clear = document.getElementById("clear");
-const equal = document.querySelector('.equal');
-const signe  = document.querySelector('.signe');
+const equal = document.querySelector(".equal");
+const signe = document.querySelector(".signe");
 
 clear.addEventListener("click", () => {
   result.textContent = "0";
@@ -16,39 +16,31 @@ buttons.forEach((button) => {
       : (result.textContent += button.id);
 
     const tableau = [...result.textContent];
-    if(tableau.length > 13){
-        alert('Limite de caractères atteinte');
-        result.textContent = result.textContent.slice(0, -1);
+    if (tableau.length > 13) {
+      alert("Limite de caractères atteinte");
+      result.textContent = result.textContent.slice(0, -1);
     }
-   
   });
 });
-
 
 clear1.addEventListener("click", () => {
   result.textContent = result.textContent.slice(0, -1);
 });
 
 equal.addEventListener("click", () => {
-    try {
-        result.textContent = eval(result.textContent);
-    } catch (error) { 
-        alert('Expression invalide');
-    }
-     const resultString = parseFloat(result.textContent).toFixed(2);
-     result.textContent = resultString;
+  try {
+    result.textContent = eval(result.textContent);
+  } catch (error) {
+    alert("Expression invalide");
+  }
+  const resultString = parseFloat(result.textContent).toFixed(2);
+  result.textContent = resultString;
+});
 
-})
-
-signe.addEventListener('click', () =>{
-    if( parseInt(result.textContent) > 0){
-        result.textContent = -parseFloat(result.textContent);
-    } else {
-         result.textContent = Math.abs(parseFloat(result.textContent));
-    }
-})
-
-
-
-
-
+signe.addEventListener("click", () => {
+  if (parseInt(result.textContent) > 0) {
+    result.textContent = -parseFloat(result.textContent);
+  } else {
+    result.textContent = Math.abs(parseFloat(result.textContent));
+  }
+});
